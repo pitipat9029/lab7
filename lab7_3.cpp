ad560592cd928b2bb456924ff9b9b87f18a1be00
Pitipat <pitipat_fu@cmu.ac.th>
2021-11-30 17:03:53 +0700

    Comite Lab7_3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -2,14 +2,46 @@
 
 using namespace std;
 
+ int adiff(int x ,int y ){
+  while ( x >= 360 )
+    {
+      x -= 360 ;
+    }
+
+  while ( y >= 360 )
+    {
+      y -= 360 ;
+    }
+
+
+int c = x-y ;
+    if( c < 0 ){
+      c = c*-1 ;
+    }
+    while ( c > 180 )
+    {
+      c = 360 - c ;
+    }
+    
+return c ;
+ }
+
 
 int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+  cout << adiff(180,270) << "\n";
+  
+  cout << adiff(210,45) << "\n";
+
+  cout << adiff(0,360)<< "\n";
+
+  cout << adiff(10,350)<< "\n";
+
+  cout << adiff(95,260)<< "\n";
+
+  cout << adiff(90,-90)<< "\n";
+
+  cout << adiff(1000,280)<< "\n";
+
+  cout << adiff(60,244)<< "\n";
+
 }
